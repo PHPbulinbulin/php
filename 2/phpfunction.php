ceb23845b4fa533b3b7b99ce484a000910911e14
PHPbulinbulin <44659541+PHPbulinbulin@users.noreply.github.com>
2019-03-10 20:33:23 +0800

    错误

--- 2/phpfunction.php
+++
@@ -1,9 +0,0 @@
-<?php
-//arsort 按最大的值排序 &降序, $arr = 数组;
-arsort($arr);
-//reset 获取数组中第一个元素的值, $arr = 数组;
-reset($arr);
-//key 获取数组中第一个元素的键, $arr = 数组;
-key($arr);
-//round 数值取余数 第一个数值, 第二个小数位数
-round(100.61152,2);
