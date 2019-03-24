101739786893e90b6ebd8dd0b56c70ad18f04e50
PHPbulinbulin <44659541+PHPbulinbulin@users.noreply.github.com>
2019-03-24 16:19:51 +0800

    Delete laravelgetSevendays.php

--- 2/laravelgetSevendays.php
+++
@@ -1,4 +0,0 @@
-  $costDatas = D::table('表名')
-                -> whereDate('time', '>', date('Y-m-d', strtotime('-7 days')))
-                 ->whereDate('time', '<=', date('Y-m-d'))
-                -> get() -> toArray();
