675b590ecb76d5ebf59a5f12ad31142674b146a8
PHPbulinbulin <44659541+PHPbulinbulin@users.noreply.github.com>
2020-04-02 20:48:46 +0800

    冒泡排序

--- 2/bubblingsort.php
+++ 2/bubblingsort.php
@@ -4,11 +4,7 @@
  * */
 function bubblingSort($arr){
     for ($n=1; $n<count($arr); $n++) {
-        //echo $n; 7个数比6次：123456
-        //步骤3：判断交换值
         for ($i=0; $i < count($arr) - $n; $i++) {
-            //echo $i . '<br />';  //123456
-            //echo $arr[$i] . '<br />';
             if ($arr[$i] > $arr[$i+1]) {
                 $temp = $arr[$i];
                 $arr[$i] = $arr[$i+1];
