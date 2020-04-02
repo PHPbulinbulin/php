02951440d2127b6a04a7ded7145f89ac28fc4184
PHPbulinbulin <44659541+PHPbulinbulin@users.noreply.github.com>
2020-04-02 20:57:05 +0800

    curl库

--- 2/curl.php
+++ 2/curl.php
@@ -1,8 +1,11 @@
 <?php
-/*
- * curl库
- * */
-function httpRequest($api, $postData = array())
+/**
+ * @param string $api url地址
+ * @param array $postData 参数
+ * @param int $timeoutSec 等待响应时间
+ * @return bool|string
+ */
+function httpRequest($api, $postData = array(), $timeoutSec = 0)
 {
     //1.初始化
     $ch = curl_init();
@@ -17,6 +20,8 @@ function httpRequest($api, $postData = array())
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
     }
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeoutSec);
+    curl_setopt($ch, CURLOPT_TIMEOUT, $timeoutSec);
     //curl注意事项，如果发送的请求是https，必须要禁止服务器端校检SSL证书
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
